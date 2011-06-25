775411873fa296a8e66f36574b4192650b2c6f4a
Akash Manohar J <akash@akash.im>
2011-06-26 00:08:11 +0530

    initial commit
    Signed-off-by: Akash Manohar J <akash@akash.im>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aride::Application.config.secret_token = '5957b89b8f13319fd633df351600301535f524f7cdcc4abae5ecdd35d4a9d5a6e42533c746352684541b371c225fb801c7b8ded9d1e8641ec8d4504bc6f1d4df'
