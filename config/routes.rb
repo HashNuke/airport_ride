2a95d37ffa62a8e204e79fe13432461511ed5dd7
Akash Manohar J <akash@akash.im>
2011-06-26 04:45:21 +0530

    minor changes
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Aride::Application.routes.draw do
 
+  get "main/index"
+  
+  root :to=>"main#index"
+
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
 
--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20110625225656) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
