149bd45109ae03b077cac22ac825efa826b6d19c
Akash Manohar J <akash@akash.im>
2011-06-26 04:46:55 +0530

    fixed db and added email fields
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :facebook, '114308221991692', 'd0760e8127dba7e2ae39c8416003df2b', {:scope => 'publish_stream,offline_access,email'}
+  provider :facebook, '114308221991692', 'd0760e8127dba7e2ae39c8416003df2b' 
+  #{:scope => 'publish_stream,offline_access,email'}
 end
