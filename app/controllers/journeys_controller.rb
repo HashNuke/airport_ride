bc458ab51c7f63b203e99b6f79517f6a3327e458
Akash Manohar J <akash@akash.im>
2011-06-26 06:16:03 +0530

    added some stuff which I don't remember anymore
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -2,8 +2,10 @@ class JourneysController < ApplicationController
   # GET /journeys
   # GET /journeys.json
   def index
+    # @journeys = Journey.where(:user_id=>current_user.id).order("created_at desc").limit(10)
+    # TODO: uncomment above line after debug and delete below line
     @journeys = Journey.all
-
+    
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @journeys }
@@ -14,15 +16,38 @@ class JourneysController < ApplicationController
   # GET /journeys/1.json
   def show
     @journey = Journey.find(params[:id])
+    
+    #TODO add another field. delete this complex calculation
+    #TODO if the journey belongs to the user, then render, else throw error
+    
+    travel_time = @journey.travel_stamp.to_s
+    t_year = travel_time[0..3].to_i
+    t_month = travel_time[4..5].to_i
+    t_day = travel_time[6..7].to_i
+    t_hour = travel_time[8..9].to_i
+    t_min = travel_time[10..11].to_i
+    
+    t_time = Time.new(t_year, t_month, t_day, t_hour, t_min)
+    from_time = t_time.change :hour=>-1, :min=>-30
+    to_time = t_time.change :hour=>1, :min=>30
 
+    to_time_string = to_time.strftime "%Y%m%d%H%M"
+    from_time_string = from_time.strftime "%Y%m%d%H%M"
+    
+    from_time_int = from_time_string.to_i
+    to_time_int = to_time_string.to_i
+
+    @possible_matches = Journey.where :travel_time => from_time_int..to_time_int
+    
+    # if json, you'll need @possible_matches and @journey in hash
+  
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @journey }
+      format.json { render json: {journey: @journey, possible_matches: @possible_matches} }
     end
   end
 
-  # GET /journeys/new
-  # GET /journeys/new.json
+
   def new
     @journey = Journey.new
 
