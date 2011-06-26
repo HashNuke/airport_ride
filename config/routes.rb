6c2cb08791f3cd9857099a24aa9cc0db4c2cce3f
Akash Manohar J <akash@akash.im>
2011-06-26 12:58:20 +0530

    commented out routes for omniauth
    Signed-off-by: Akash Manohar J <akash@akash.im>

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Aride::Application.routes.draw do
   
   root :to=>"main#index"
 
-  match "/auth/:provider/callback" => "sessions#create"
-  match "/signout" => "sessions#destroy", :as => :signout
+  # match "/auth/:provider/callback" => "sessions#create"
+  # match "/signout" => "sessions#destroy", :as => :signout
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
